5879fda125a2b82c4e5ee5fea81a21a3fb767b3b
Ryan Mansfield <ryanmansfield86@gmail.com>
2019-09-06 16:24:51 -0300

    fixed the test errors

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,14 +1,16 @@
 class DosesController < ApplicationController
   before_action :set_cocktail, except: :destroy
+  before_action :set_dose, only: [:destroy, :show]
 
   def new
     @dose = Dose.new
+    @ingredients = Ingredient.all
   end
 
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-    @dose.ingredient = Ingredient.find(params['dose']['ingredient'])
+    # @dose.ingredient = Ingredient.find(params['dose']['ingredient'])
 
     if @dose.save
       redirect_to @cocktail
@@ -30,10 +32,15 @@ class DosesController < ApplicationController
   def dose_params
     # *Strong params*: You need to *whitelist* what can be updated by the user
     # Never trust user data!
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 
   def set_cocktail
     @cocktail = Cocktail.find(params[:cocktail_id])
   end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,5 @@
 class Ingredient < ApplicationRecord
   validates :name, uniqueness: true, presence: true, allow_blank: false
   has_many :doses
+  has_many :cocktails, through: :doses
 end
