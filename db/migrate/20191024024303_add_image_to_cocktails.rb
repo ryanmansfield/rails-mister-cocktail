eacede1af894abf35fe08ce374606bd892d1bb18
Ryan Mansfield <ryanmansfield86@gmail.com>
2019-10-23 23:43:33 -0300

    Added image to cocktails table

---
+++ db/migrate/20191024024303_add_image_to_cocktails.rb
@@ -0,0 +1,4 @@
+class AddImageToCocktails < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_143806) do
+ActiveRecord::Schema.define(version: 2019_10_24_024303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
