bdc48906eee3b886c38f38a5096b1ea0e385baf7
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-02-18 22:58:45 -0600

    cloudinary upload debugging

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 #   # include CarrierWave::MiniMagick
 
 #   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
 #   # storage :fog
 
 #   # Override the directory where uploaded files will be stored.
@@ -15,9 +15,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def cache_dir
-    '/tmp/rails-mister-cocktail-cache'
-  end
+  # def cache_dir
+  #   '/tmp/rails-mister-cocktail-cache'
+  # end
 #   # Provide a default URL as a default if there hasn't been a file uploaded:
 #   # def default_url(*args)
 #   #   # For Rails 3.1+ asset pipeline compatibility:
---
+++ config/cloudinary.rb
@@ -0,0 +1,7 @@
+# Cloudinary.config do |config|
+#  config.cloud_name = ‘ur cloud’
+#  config.api_key = ‘ur key’
+#  config.api_secret = ‘ur secret’
+#  config.secure = true
+#  config.cdn_subdomain = true
+# end
